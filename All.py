5a78410bc008a74e6d82bd1bc933b9030b1acf79
huveewomg <wongrenthou1265@gmail.com>
2023-07-01 22:26:18 +0800

    recep menu func update + fixed

--- All.py
+++ All.py
@@ -203,24 +203,25 @@ def AdminMenu(): #Admin Menu All function for admin
 def RecepMenu(): #Reception Menu 
     while True:
         print('Receptionist Menu')
-        print("1. Register new Student ")
-        print("2. Edit Tutor")
-        print("3. Income Report")
+        print("1. Register / Delete Student ")
+        print("2. Update Student's enrollment")
+        print("3. Payment")
         print("4. Change Password")
+
         choice = int(input("Enter your choice: "))
         if choice == 1:
-            EditMenu('Receptionist')
+            EditMenu('Student')
         elif choice == 2:
-            EditMenu('Tutor')
+            UpdateMenu('Student')
         elif choice == 3:
-            Income()
+            PaymentMenu()
         elif choice == 4:
-            ChangePW('Admin')
+            ChangePW('Receptionist')
         else:
             print('Invalid input,please enter number 1 to 4 only.')
             print("")
 
-
+def 
 
 
 
